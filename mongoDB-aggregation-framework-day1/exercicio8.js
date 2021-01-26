8ede200faefe0eb4e1cac8cc5651eec4e41e09d2
Leandro <leandro.feitoza8018@gmail.com>
2021-01-26 18:53:35 -0300

    exercicio8

--- mongoDB-aggregation-framework-day1/exercicio8.js
+++ mongoDB-aggregation-framework-day1/exercicio8.js
@@ -1,2 +1,26 @@
 // Exercício 8 : Descubra quais são os 5 clientes que gastaram o maior valor.
 
+
+db.vendas.aggregate([
+  { $match: { status: "ENTREGUE" } },
+  { 
+    $group: {
+      _id: "$clienteId",
+      valorTotal: {
+        $sum: "$valorTotal"
+      } 
+    } 
+  },
+  { $sort: { valorTotal: -1 } },
+  { $limit: 5 }
+]);
+
+// db.vendas.aggregate([
+//   { $match: { status: { $ne: "ENTREGUE"} } },
+  
+// ]);
+
+// db.vendas.aggregate([
+//   { $match: { status:{ $in: ["ENTREGUE", "EM SEPARAÇÃO"] }} },
+  
+// ]);
