ad78c90def09cac1ee10188ac288ca9a756e1d66
Leandro Feitoza <leandro.feitoza8018@gmail.com>
2021-02-01 20:44:58 -0300

    exercicio 2

--- exercises/introducao-ao-desenvolvimento-web-com-node-parte1/exercicio/index.js
+++ exercises/introducao-ao-desenvolvimento-web-com-node-parte1/exercicio/index.js
@@ -1,11 +1,44 @@
-// O script deve calcular o IMC de uma pessoa e exibi-lo na tela. Crie um novo pacote chamado calcula-imc , e utilize o script start do npm para executar nosso arquivo index.js . Siga utilizando o pacote readline-sync para coletar os dados do usuário. A fórmula para cálculo do IMC é peso / altura² .
-// Observação : Lembre-se que peso e altura não são sempre números inteiros e, portanto, não podemos utilizar a função questionInt do pacote readline-sync . No entanto, o mesmo pacote possui uma função para tratar números com casas decimais. Encontre essa função na documentação do readline-sync e não se esqueça de utilizar o toFixed(2) nos números decimais! Dica: Para utilizar o npm start , lembre-se de configurar seu package.json , adicionando a seus scripts um start: "node index.js" , por exemplo. Nesse exemplo, o index.js representa o arquivo main do seu package.json .
+// Agora, modifique o script acima para que ele informe se a pessoa, cujo IMC foi calculado, possui algum nível de obesidade. Considere a seguinte tabela para saber qual situação deve ser apresentada para cada resultado:
+// IMC	Situação
+// Abaixo de 18,5	Abaixo do peso (magreza)
+// Entre 18,5 e 24,9    	Peso normal
+// Entre 25,0 e 29,9	Acima do peso (sobrepeso)
+// Entre 30,0 e 34,9	Obesidade grau I
+// Entre 35,0 e 39,9	Obesidade grau II
+// 40,0 e acima	Obesidade graus III e IV
 
 const readlineSync = require('readline-sync');
 
-const peso = readlineSync.questionFloat("Qual o seu peso?");
-const altura = readlineSync.questionFloat("Qual sua altura?");
-const IMC = (peso / (Math.pow(altura, 2)))
 
-console.log("Seu IMC é: %s", IMC.toFixed(2))
+function calculateIMC() {
+  const peso = readlineSync.questionFloat("Qual o seu peso?");
+  const altura = readlineSync.questionFloat("Qual sua altura?");
+  const IMC = (peso / (Math.pow(altura, 2)))
+  console.log("Seu IMC é: %s", IMC.toFixed(2))
+  if (IMC < 18.5 ){
+    console.log("Abaixo do peso (magreza)")
+    return;
+  }
+  if (IMC >= 18.50 && IMC <= 24.99) {
+    console.log("Peso Normal")
+    return;
+  }
+  if (IMC >= 25.0 && IMC <= 29.9) {
+    console.log("Acima do peso (sobrepeso)")
+    return;
+  }
+  if (IMC >= 30.0 && IMC <= 34.9) {
+    console.log("Obesidade grau I")
+    return;
+  }
+  if (IMC >= 35.0 && IMC <= 39.9) {
+    console.log("Obesidade grau II")
+    return;
+  }
+  if (IMC > 40) {
+    console.log("Obesidade graus III e IV")
+    return;
+  }
+}
 
+calculateIMC();
