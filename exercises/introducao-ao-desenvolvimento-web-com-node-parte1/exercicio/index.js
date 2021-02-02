857afceb741e9078cf333a7f6e3b70d22ceed711
Leandro Feitoza <leandro.feitoza8018@gmail.com>
2021-02-02 00:21:45 -0300

    Refatorado o codigo do exercicio 3 - mensagem do IMC

--- exercises/introducao-ao-desenvolvimento-web-com-node-parte1/exercicio/index.js
+++ exercises/introducao-ao-desenvolvimento-web-com-node-parte1/exercicio/index.js
@@ -28,41 +28,7 @@ const questions = [
   },
 ]
 
-inquirer.prompt(questions).then((answers) => {
+inquirer.prompt(questions)
+.then((answers) => {
   const IMC = calculateIMC(answers.peso, answers.altura)
-  console.log(`Seu IMC é %s`, IMC.toFixed(2))
-})
-
-// function calculateIMC() {
-//   const peso = readlineSync.questionFloat("Qual o seu peso?");
-//   const altura = readlineSync.questionFloat("Qual sua altura?");
-
-//   const IMC = (peso / (Math.pow(altura, 2)))
-//   console.log("Seu IMC é: %s", IMC.toFixed(2))
-//   if (IMC < 18.5 ){
-//     console.log("Abaixo do peso (magreza)")
-//     return;
-//   }
-//   if (IMC >= 18.50 && IMC <= 24.99) {
-//     console.log("Peso Normal")
-//     return;
-//   }
-//   if (IMC >= 25.0 && IMC <= 29.9) {
-//     console.log("Acima do peso (sobrepeso)")
-//     return;
-//   }
-//   if (IMC >= 30.0 && IMC <= 34.9) {
-//     console.log("Obesidade grau I")
-//     return;
-//   }
-//   if (IMC >= 35.0 && IMC <= 39.9) {
-//     console.log("Obesidade grau II")
-//     return;
-//   }
-//   if (IMC > 40) {
-//     console.log("Obesidade graus III e IV")
-//     return;
-//   }
-// }
-
-// calculateIMC();
+});
